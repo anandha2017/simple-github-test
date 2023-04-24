3c8cc6e9ae9c7c5331895a92e63a36431b6a6998
Anandha Ponnampalam <github2017@ponnampalam.ca>
2023-04-24 17:25:56 +0100

    Test again

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 # This program prints Hello, world!
 
 print('Hello, world!')
+
+#q: what does SOLID stand for in OO programming
+#A: Single responsibility, Open-closed, Liskov substitution, Interface segregation, Dependency inversion
+#q: what is the difference between a class and an object
+#A: A class is a blueprint for an object. An object is an instance of a class.
+#q: what is the difference between a class and a module
+#A: A class is a blueprint for an object. A module is a file containing Python definitions and statements.
+#q: what is the difference between a class and a package
+#A: A class is a blueprint for an object. A package is a collection of modules.
+#q: what is the difference between a class and a library
+#A: A class is a blueprint for an object. A library is a collection of packages.
